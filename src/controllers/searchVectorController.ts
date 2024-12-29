f77140537275326fe39db08adc88f39a1185ebce
hopkins385 <stadhouders.sven@gmail.com>
2024-12-29 14:13:54 +0100

    error handling

--- src/controllers/searchVectorController.ts
+++ src/controllers/searchVectorController.ts
@@ -5,7 +5,11 @@ import { SearchVectorRequest } from '../schemas/searchVectorSchema';
 export const searchVectorController = async (req: Request, res: Response) => {
   const { query, recordIds } = req.body as SearchVectorRequest;
 
-  const response = await embeddingService.searchDocsByQuery({ query, recordIds });
-
-  res.json(response);
+  try {
+    const response = await embeddingService.searchDocsByQuery({ query, recordIds });
+    res.json(response);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ status: 'error' });
+  }
 };
--- src/controllers/tokenizeController.ts
+++ src/controllers/tokenizeController.ts
@@ -4,6 +4,12 @@ import { TokenizeRequest } from '../schemas/tokenizeRequestSchema';
 
 export const tokenizeController = async (req: Request, res: Response) => {
   const { text } = req.body as TokenizeRequest;
-  const token = await tokenizerService.getTokens(text);
-  res.json(token);
+
+  try {
+    const token = await tokenizerService.getTokens(text);
+    res.json(token);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ status: 'error' });
+  }
 };
