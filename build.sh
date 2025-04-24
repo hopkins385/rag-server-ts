e5836c6cb7c91f3acebb163be65793824e9e9f93
hopkins385 <stadhouders.sven@gmail.com>
2025-04-24 18:12:54 +0200

    [enh] improved build

--- build.sh
+++ build.sh
@@ -17,30 +17,23 @@ while getopts "y" opt; do
   esac
 done
 
-# Get Version from package.json
-VERSION=$(jq -r '.version' package.json)
-if [ -z "$VERSION" ]; then
-  echo "Error: Version not found in package.json"
-  exit 1
-fi
-echo "Version: $VERSION"
-
 # Configuration
 IMAGE_NAME="ghcr.io/hopkins385/rag-server"
 TAG="latest"
 DOCKERFILE="Dockerfile"
 
-echo "Build successful! Image: $IMAGE_NAME:$TAG"
+# Print status
+echo "Building $IMAGE_NAME:$TAG..."
 
-# Build the versioned image
-VERSIONED_IMAGE_NAME="$IMAGE_NAME:$VERSION"
-echo "Building versioned image $VERSIONED_IMAGE_NAME..."
+# Build image
+#   --no-cache \
 docker build \
-  --tag "$VERSIONED_IMAGE_NAME" \
+  --tag "$IMAGE_NAME:$TAG" \
   --file "$DOCKERFILE" \
   --platform linux/amd64 \
   .
-echo "Versioned image build successful! Image: $VERSIONED_IMAGE_NAME"
+
+echo "Build successful! Image: $IMAGE_NAME:$TAG"
 
 # Handle image push confirmation
 if [[ "$AUTO_CONFIRM" == true ]]; then
@@ -55,8 +48,7 @@ if [[ "$PUSH_CONFIRMATION" =~ ^[Yy]$ ]]; then
   # Push to registry
   echo $CR_PAT | docker login ghcr.io -u hopkins385 --password-stdin
   docker push "$IMAGE_NAME:$TAG"
-  docker push "$VERSIONED_IMAGE_NAME"
-  echo "Pushed $IMAGE_NAME:$TAG and $VERSIONED_IMAGE_NAME to registry."
+  echo "Image successfully pushed to registry."
 else
   echo "Image push canceled."
 fi
